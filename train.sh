fc48bd99c0f9839df1ec60937581ada38619c420
Kira Selby <kaselby@uwaterloo.ca>
2023-06-01 12:54:31 -0400

    script

--- train.sh
+++ train.sh
@@ -12,7 +12,7 @@ BATCH_PER_GPU=16
 
 
 python train.py --outdir=./training-runs/pokemon --cfg=stylegan3-t --data=./data/pokemon16.zip \
-    --gpus=$SBATCH_GPUS --batch=$BATCH_PER_GPU * $SBATCH_GPUS --mirror=1 --snap 10 --batch-gpu $BATCH_PER_GPU \
-     --kimg 10000 --cbase 16384 --cmax 256 --syn_layers 7 --workers $SBATCH_CPUS_PER_GPU * $SBATCH_GPUS
+    --gpus=$SLURM_GPUS --batch=$BATCH_PER_GPU * $SLURM_GPUS --mirror=1 --snap 10 --batch-gpu $BATCH_PER_GPU \
+     --kimg 10000 --cbase 16384 --cmax 256 --syn_layers 7 --workers $SLURM_CPUS_PER_GPU * $SLURM_GPUS
 
 
