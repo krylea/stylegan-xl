2754610835aa729faf7169b802ebb5390e96714e
Kira Selby <kaselby@uwaterloo.ca>
2023-06-20 11:49:07 -0400

    resume stem

--- train_stem.sh
+++ train_stem.sh
@@ -12,6 +12,8 @@ BATCH_PER_GPU=16
 
 RES=$1
 DATASET_NAME=$2
+ckpt=${3:-''}
+kimg=${4:-10000}
 
 BATCH=$((BATCH_PER_GPU * SLURM_GPUS_ON_NODE)) 
 GPUS=$SLURM_GPUS_ON_NODE
@@ -20,19 +22,23 @@ CPUS=$((SLURM_CPUS_PER_GPU * SLURM_GPUS_ON_NODE))
 
 if [[ $DATASET_NAME == 'imagenet' ]]
 then
-    python train.py --outdir=./training-runs/$DATASET_NAME --cfg=stylegan3-t --data=/scratch/hdd001/datasets/imagenet/train --dataset_name $DATASET_NAME \
+    argstring="--outdir=./training-runs/$DATASET_NAME --cfg=stylegan3-t --data=/scratch/hdd001/datasets/imagenet/train --dataset_name $DATASET_NAME \
         --gpus=$GPUS --batch=$BATCH --mirror=1 --snap 10 \
-        --batch-gpu $BATCH_PER_GPU --kimg 10000 --syn_layers 10 --workers $CPUS \
-        --restart_every 36000 --resolution $RES \
-        --path_stem training-runs/$DATASET_NAME/$PREFIX-stylegan3-t-${DATASET_NAME}${PREV_RES}-gpus$GPUS-batch$BATCH/best_model.pkl 
+        --batch-gpu $BATCH_PER_GPU --kimg $kimg --syn_layers 10 --workers $CPUS \
+        --resolution $RES"
 
 else
-    python train.py --outdir=./training-runs/$DATASET_NAME --cfg=stylegan3-t --data=./data/${DATASET_NAME}${RES}.zip --dataset_name $DATASET_NAME \
+    argstring="--outdir=./training-runs/$DATASET_NAME --cfg=stylegan3-t --data=./data/${DATASET_NAME}${RES}.zip --dataset_name $DATASET_NAME \
         --gpus=$GPUS --batch=$BATCH --mirror=1 --snap 10 \
-        --batch-gpu $BATCH_PER_GPU --kimg 10000 --syn_layers 7 --workers $CPUS \
-        --cbase 16384 --cmax 256 --restart_every 36000 --resolution $RES 
+        --batch-gpu $BATCH_PER_GPU --kimg $kimg --syn_layers 7 --workers $CPUS \
+        --cbase 16384 --cmax 256 --resolution $RES" 
 fi
 
+if [ -z $ckpt ]
+then
+    argstring="$argstring --resume $ckpt"
+fi
 
+python train.py $argstring
 
 
