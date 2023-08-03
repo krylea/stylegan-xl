e6ccc3d60b0f53f76ce65d970574b55068c7c10c
ziyuwang <ziyuwang@v.cluster.local>
2023-08-03 11:08:54 -0400

    small changes

--- train_stem.sh
+++ train_stem.sh
@@ -31,7 +31,7 @@ GPUS=$SLURM_GPUS_ON_NODE
 CPUS=$((SLURM_CPUS_PER_GPU * SLURM_GPUS_ON_NODE))
 
 # additional environment variables for distributed training
-export WORLD_SIZE=$SLURM_NPROCS
+export WORLD_SIZE=8
 export RANK=$SLURM_PROCID
 export LOCAL_RANK=$SLURM_LOCALID
 export MASTER_ADDR=$(srun --ntasks=1 hostname 2>&1 | tail -n1)
