3f133735a7e5acc0942f2bed8a0d1cc0d8dc5044
colbyziyuwang <112201493+colbyziyuwang@users.noreply.github.com>
2023-08-03 14:59:31 -0400

    multi node training

--- train_stem.sh
+++ train_stem.sh
@@ -2,8 +2,9 @@
 #SBATCH --job-name=stylegan-xl
 #SBATCH --output=logs/slurm-%j.txt
 #SBATCH --open-mode=append
-#SBATCH --ntasks=1
-#SBATCH --gres=gpu:4
+#SBATCH --nodes=$6  # number of nodes
+#SBATCH --ntasks-per-node=1  # number of tasks per node
+#SBATCH --gres=gpu:4  # number of gpus per node
 #SBATCH --partition=a40
 #SBATCH --cpus-per-gpu=1
 #SBATCH --mem=100GB
@@ -18,18 +19,23 @@ desc=${5:-''}
 
 if [[ -z $SLURM_CPUS_PER_GPU ]]
 then
-    SLURM_CPUS_PER_GPU=2
+    SLURM_CPUS_PER_GPU=1
 fi
 if [[ -z $SLURM_GPUS_ON_NODE ]]
 then
-    SLURM_GPUS_ON_NODE=2
+    SLURM_GPUS_ON_NODE=1
 fi
 
-BATCH=$((BATCH_PER_GPU * SLURM_GPUS_ON_NODE)) 
+BATCH=$((BATCH_PER_GPU * SLURM_GPUS_ON_NODE))
 GPUS=$SLURM_GPUS_ON_NODE
 CPUS=$((SLURM_CPUS_PER_GPU * SLURM_GPUS_ON_NODE))
 
-
+# additional environment variables for distributed training
+export WORLD_SIZE=$(($6 * $SLURM_GPUS_ON_NODE))
+export RANK=$SLURM_PROCID
+export LOCAL_RANK=$SLURM_LOCALID
+export MASTER_ADDR=$(srun --ntasks=1 hostname 2>&1 | tail -n1)
+export MASTER_PORT=12355
 
 if [[ $DATASET_NAME == 'imagenet' ]]
 then
@@ -42,7 +48,7 @@ else
     argstring="--outdir=./training-runs/$DATASET_NAME --cfg=stylegan3-t --data=./data/${DATASET_NAME}${RES}.zip --dataset_name $DATASET_NAME \
         --gpus=$GPUS --batch=$BATCH --mirror=1 --snap 10 \
         --batch-gpu $BATCH_PER_GPU --kimg $kimg --syn_layers 7 --workers $CPUS \
-        --cbase 16384 --cmax 256 --resolution $RES" 
+        --cbase 16384 --cmax 256 --resolution $RES"
 fi
 
 if [[ -n $ckpt ]]
@@ -57,5 +63,4 @@ fi
 
 
 python train.py $argstring
-
 
