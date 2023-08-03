0cc16fda69ae6b6108bee35f399ae166afc4d1b9
colbyziyuwang <112201493+colbyziyuwang@users.noreply.github.com>
2023-08-03 15:33:47 -0400

    Update train_superres2.sh

--- train_superres2.sh
+++ train_superres2.sh
@@ -2,8 +2,9 @@
 #SBATCH --job-name=stylegan-xl
 #SBATCH --output=logs/slurm-%j.txt
 #SBATCH --open-mode=append
-#SBATCH --ntasks=1
-#SBATCH --gres=gpu:4
+#SBATCH --nodes=1-2  # number of nodes
+#SBATCH --ntasks-per-node=1  # number of tasks per node
+#SBATCH --gres=gpu:4  # number of gpus per node
 #SBATCH --partition=a40
 #SBATCH --cpus-per-gpu=1
 #SBATCH --mem=100GB
@@ -17,6 +18,7 @@ PREV_RES=$((RES / 2))
 ckpt=${4:-''}
 kimg=${5:-10000}
 desc=${6:-''}
+MASTER_PORT=$7
 
 if [[ -z $SLURM_CPUS_PER_GPU ]]
 then
@@ -31,7 +33,12 @@ BATCH=$((BATCH_PER_GPU * SLURM_GPUS_ON_NODE))
 GPUS=$SLURM_GPUS_ON_NODE
 CPUS=$((SLURM_CPUS_PER_GPU * SLURM_GPUS_ON_NODE))
 
-
+# additional environment variables for distributed training
+export WORLD_SIZE=$(($SLURM_JOB_NUM_NODES * $SLURM_GPUS_ON_NODE))
+export RANK=$SLURM_PROCID
+export LOCAL_RANK=$SLURM_LOCALID
+export MASTER_ADDR=$(srun --ntasks=1 hostname 2>&1 | tail -n1)
+export MASTER_PORT
 
 if [[ $DATASET_NAME == 'imagenet' ]]
 then
